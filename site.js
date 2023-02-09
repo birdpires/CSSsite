b2ed4f03ea956c3a6c798efb772f94b25c59e0d8
Rodrygo <113386711+birdpires@users.noreply.github.com>
2023-02-09 14:32:07 -0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,4 @@
+function exibealert() {
+    alert("EU AVISEI! A partir de agora VOCÊ que apertou esse botão tá me devendo um açaí!! Eu irei cobrar!!")
+}
+/*o java ta aqui só pra piada kjkkkkkkkkkk desculpa*/
